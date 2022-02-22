48afff0f8bc11181f7c40c30f6429a5160a8ba4c
alxval <avalencia190@misena.edu.co>
2022-02-22 15:11:45 -0500

    Creamos la función de simular la caminata y la función de graficar en el enter point o archivo principal

--- randomtrail.py
+++ randomtrail.py
@@ -1,5 +1,6 @@
 import imp
-from wandering import commonWandering
+from turtle import title
+from wandering import commonWandering, wandering
 from trail import trail
 from location import location
 
@@ -13,3 +14,20 @@ def walking(location, wandering, steps):
 
         return begin.distance(location.get_location(wandering))
 
+def simulate_walk(steps, atts, type_wandering):
+    wandering = type_wandering(name='Alirio')
+    origin = location(0,0)
+    distances = []
+
+    for _ in range(atts):
+        trail = trail()
+        trail.add_wandering(wandering, origin)
+        sim_walk = walking(trail, wandering, steps)
+        distances.append(round(sim_walk,1))
+    
+    return distances
+
+def graph(x, y):
+    grph = figure(title='Camino del errante', x_axis_label='Pasos', y_axis_label='Distancia')
+    grph.line(x, y, legend='Distancia')
+    show(grph)
