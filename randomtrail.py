5559675bfd5720691de172c89c264fd0411ee88d
alxval <avalencia190@misena.edu.co>
2022-03-15 17:53:07 -0500

    Mapa del wandering con 10000 pasos

--- randomtrail.py
+++ randomtrail.py
@@ -1,53 +1,71 @@
 from turtle import distance, title
+from typing import final
 from wandering import commonWandering, wandering
 from trail import Trail
 from location import Location
 
 from bokeh.plotting import figure, output_file, show
 
-def walking(location, wandering, steps):
-    begin = location.get_location(wandering)
+def typewanderingNow(type_wandering):
+    if type_wandering.__name__ == "commonWandering":
+        return "Errante común"
+    elif type_wandering.__name__ == "wanderingRight":
+        return "Errante derechista"
+    else:
+        return "Errante izquierdista"
 
-    for _ in range(steps):
-        location.move_wandering(wandering)
+def walking(wandering, steps, type_wandering):
+    begin = [wandering.position()]
 
-    return begin.distance(location.get_location(wandering))
+    x_grph = [0]
+    y_grph = [0]
+
+    for _ in range(steps-1):
+        wandering.walk()
+        x, y = wandering.position()
+        x_grph.append(x)
+        y_grph.append(y)
+
+    typeNow = typewanderingNow(type_wandering)
+    graph_steps(x_grph, y_grph, typeNow, steps)
+    return wandering.dist_origin()
 
 def simulate_walk(steps, atts, type_wandering):
-    wandering = type_wandering(name='Alirio')
-    origin = Trail(0,0)
+    wandering = []
     distances = []
 
-    for _ in range(atts):
-        location = Location()
-        location.add_wandering(wandering, origin)
-        sim_walk = walking(location, wandering, steps)
+    for i in range(atts):
+        wandering.append(type_wandering(name=f'Rasputin {i}'))
+        sim_walk = walking(wandering[i], steps, type_wandering)
         distances.append(round(sim_walk,1))
     return distances
 
-def graph(x, y):
-    grph = figure(title='Camino del errante', x_axis_label='Pasos', y_axis_label='Distancia')
-    grph.line(x, y, legend='Distancia')
+def graph_steps(x_graph, y_graph, type_wandering, steps):
+    grph = figure(title=type_wandering, x_axis_label='Pasos', y_axis_label='Distancia')
+    grph.line(x_graph, y_graph, legend_label=str(steps)+' pasos')
+    final_x = x_graph[-1]
+    final_y = y_graph[-1]
+    grph.diamond_cross(0, 0, fill_color="green", line_color="green", size=18)
+    grph.diamond_cross(final_x, final_y, fill_color="red", line_color="red", size=18)
+    str_finalx = [0, final_x]
+    str_finaly = [0, final_y]
+    grph.line(str_finalx, str_finaly, line_width=2, color="blue")
     show(grph)
 
 def main(walk_dist, atts, type_wandering):
-    avgwalk_dist = []
 
     for steps in walk_dist:
         distances = simulate_walk(steps, atts, type_wandering)
         avgdist = round(sum(distances) / len(distances), 4)
         maxdist = max(distances)
         mindist = min(distances)
-        avgwalk_dist.append(avgdist)
         print(f'{type_wandering.__name__} Caminata aleatoria de {steps} pasos')
         print(f'Media = {avgdist}')
         print(f'Max = {maxdist}')
         print(f'Min = {mindist}')
-    
-    graph(walk_dist, avgwalk_dist)
 
 if __name__ == '__main__':
 
-    walk_dist = [10, 100, 1000, 10000]
-    atts = 100
+    walk_dist = [10000]
+    atts = 1
     main(walk_dist, atts, commonWandering)
