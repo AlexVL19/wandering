b13b958da70f37b2c832019ab96011acb3c83047
alxval <avalencia190@misena.edu.co>
2022-03-15 13:24:59 -0500

    Cambio 1

--- randomtrail.py
+++ randomtrail.py
@@ -1,11 +1,3 @@
-import imp
-from wandering import commonWandering
-from trail import trail
-from location import location
-
-from bokeh.plotting import figure, output_file, show
-
-
 from turtle import distance, title
 from wandering import commonWandering, wandering
 from trail import Trail
